396a8e3c925ebedef143cea04d7543188da1686e
Jacob Waro <dwaro@wisc.edu>
2017-02-19 16:46:59 -0600

    Work on Assignment One
    Panel content now updates through the sequence and some added styling.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@
 //initialize function called when the script loads
 function initialize(){
 
-    //cities();
-    //debugAjax();
+    cities();
     createMap();
 
 };
@@ -287,7 +286,7 @@ function createPropSymbols(data, map, attributes){
 //Step 1: Create new sequence controls
 function createSequenceControls(map, attributes){
   //create range input element (slider)
-  $('#infoPanel').append('<input class="range-slider" type="range">');
+  $('#slider').append('<input class="range-slider" type="range">');
 
   //set slider attributes
   $('.range-slider').attr({
@@ -298,12 +297,12 @@ function createSequenceControls(map, attributes){
   });
 
   // step / reverse button
-  $('#infoPanel').append('<button class="skip" id="reverse">Reverse</button>');
+  $('#slider').append('<button class="skip" id="reverse">Reverse</button>');
   // Adds reverse image.
   //$('#reverse').html('<img src="img/reverse.png">');
 
   // step / forward button
-  $('#infoPanel').append('<button class="skip" id="forward">Skip</button>');
+  $('#slider').append('<button class="skip" id="forward">Skip</button>');
   // Adds forward image.  Right arrow by Guilhem from the Noun Project
   //$('#forward').html('<img src="lib/images/forward.png">');
 
@@ -355,20 +354,35 @@ function updatePropSymbols(map, attribute){
               layer.setRadius(radius);
             }
 
-            //layer.setRadius(radius);
-
             //add country to popup content string
-            var popupContent = "<p><b>Country:</b> " + props.Country + "</p>";
-
+            var panelContent = "<p><b>Country:</b> " + props.Country + "</p>";
 
             //add formatted attribute to panel content string
             var year = attribute.split("_")[2];
-            popupContent += "<p><b>Population growth from " + year + ":</b> "
-              + props[attribute] + "</p>";
+            var yearDos = attribute.split("_")[3];
+
+            panelContent += "<p><b>Population growth from " + year + " to " + yearDos + ":</b> "
+            + parseFloat(((props[attribute]) - 1) * 100).toFixed(2) + "% </p>";
+
+            //popup content is now just the city name
+            var popupContent = props.Country;
 
             //replace the layer popup
             layer.bindPopup(popupContent, {
                 offset: new L.Point(0,-radius)
+            });
+
+            //event listeners to open popup on hover
+            layer.on({
+                mouseover: function(){
+                    this.openPopup();
+                },
+                mouseout: function(){
+                    this.closePopup();
+                },
+                click: function(){
+                  $("#infoPanel").html(panelContent);
+                }
             });
         };
     });
@@ -458,4 +472,79 @@ $(document).ready(initialize);
 //   $("#mydiv").append('<br>GeoJSON data: </br>'  + JSON.stringify(response));
 //
 // // close for debugCallback
+// };
+
+// //function to convert markers to circle markers
+// function pointToLayer(feature, latlng, attributes){
+//     //Determine which attribute to visualize with proportional symbols
+//     var attribute = attributes[0];
+//
+//     //create marker options
+//     var options = {
+//       fillColor: "rgb(0, 255, 204)",
+//       color: "#000",
+//       weight: 1,
+//       opacity: 1,
+//       fillOpacity: 0.4
+//     };
+//
+//     var popDecline = {
+//       fillColor: "rgb(128,0,0)",
+//       color: "#000",
+//       weight: 1,
+//       opacity: 1,
+//       fillOpacity: 0.4
+//     };
+//
+//     //For each feature, determine its value for the selected attribute
+//     var attValue = (Number(feature.properties[attribute]) - 1);
+//
+//     if (attValue > 0) {
+//       options.radius = calcPropRadius((attValue) * 100);
+//       var layer = L.circleMarker(latlng, options);
+//     } else {
+//       attValue = (1 - Number(feature.properties[attribute]));
+//       popDecline.radius = calcPropRadius((attValue) * 100);
+//       var layer = L.circleMarker(latlng, popDecline);
+//     };
+//
+//     //build popup content string starting with city...Example 2.1 line 24
+//     var panelContent = "<p><b>Country:</b> " + feature.properties.Country + "</p>";
+//
+//     //add formatted attribute to popup content string
+//     var yearOne = attribute.split("_")[2];
+//     var yearTwo = attribute.split("_")[3];
+//
+//     panelContent += "<p><b>Population growth from " + yearOne + " to "
+//       + yearTwo + ":</b> " + parseFloat(((feature.properties[attribute]) - 1) * 100).toFixed(2)
+//       + "% </p>";
+//
+//     //popup content is now just the city name
+//     var popupContent = feature.properties.Country;
+//
+//     //bind the popup to the circle marker
+//     layer.bindPopup(popupContent, {
+//
+//         // positions the popup above in a consistent space
+//         offset: new L.Point(0,-options.radius, -popDecline.radius),
+//
+//         // gets rid of close button on the popup
+//         closeButton: false
+//     });
+//
+//     //event listeners to open popup on hover
+//     layer.on({
+//       mouseover: function(){
+//         this.openPopup();
+//       },
+//       mouseout: function(){
+//         this.closePopup();
+//       },
+//       click: function(){
+//             $("#infoPanel").html(panelContent);
+//       }
+//     });
+//
+//     //return the circle marker to the L.geoJson pointToLayer option
+//     return layer;
 // };
